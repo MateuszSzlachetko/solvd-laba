4be15009e5e91134fcb5b8d1704d59a9b39b4bbe
Mateusz Szlachetko <mateusz.szlachetko@gmail.com>
2024-03-29 22:46:32 +0100

    chore: add git ignore, jest and jest.config

---
+++ hw1/jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('jest').Config} */
+const config = {
+  coverageProvider: "v8",
+  moduleFileExtensions: [
+    "js",
+    "mjs",
+    "cjs",
+    "jsx",
+    "ts",
+    "tsx",
+    "json",
+    "node"
+  ],
+};
+
+module.exports = config;
