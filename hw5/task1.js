caefd38724350a0cb45b3276b02ee1c303c27e44
Mateusz Szlachetko <mateusz.szlachetko@gmail.com>
2024-04-29 12:56:04 +0200

    fix: fixed task1 logic

--- hw5/task1.js
+++ hw5/task1.js
@@ -2,14 +2,16 @@ function customFilterUnique(array, callback) {
   const unique = new Set();
   return array.filter(item => {
     const value = callback(item);
-    if (unique.has(value)) {
-      return false;
-    } else {
-      unique.add(value);
+    const identifier = JSON.stringify(value);
+    if (value !== undefined && !unique.has(identifier)) {
+      unique.add(identifier);
       return true;
+    } else {
+      return false;
     }
   });
 }
+
 
 const data = [
   {id: 1, name: "A"},
@@ -22,3 +24,18 @@ const data = [
 
 const uniqueByName = customFilterUnique(data, item => item.name);
 console.log(uniqueByName);
+let arrayOfObjects = [
+  {a: 1, b: 2},
+  {a: 1, b: 2},
+  {c: 3, d: 4},
+  {a: 5, b: 6},
+  {e: 7, f: 8},
+  {g: 9, h: 0}
+];
+
+const filterByPropertyA = (obj) => {
+  if (!obj || typeof obj !== "object") throw new Error("pass an object as parameter")
+  return Object.keys(obj).includes("a") ? obj : undefined
+}
+
+console.log(customFilterUnique(arrayOfObjects, filterByPropertyA))
