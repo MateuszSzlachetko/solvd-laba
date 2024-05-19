a554763cac69052316cf073b2898a44816acfeee
Mateusz Szlachetko <mateusz.szlachetko@gmail.com>
2024-05-19 16:26:58 +0200

    feat: add queue class

---
+++ hw9/Queue.js
@@ -0,0 +1,35 @@
+class Queue {
+  constructor(size) {
+    this.size = size;
+    this.array = [];
+  }
+
+  enqueue(element) {
+    if (this.size > this.array.length) this.array.push(element)
+    else console.error("Queue is full")
+  }
+
+  dequeue() {
+    return this.array.shift();
+  }
+
+  peek() {
+    return this.array[0];
+  }
+}
+
+const queue = new Queue(5);
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+console.log(queue.dequeue()); // 1
+console.log(queue.dequeue()); // 2
+console.log(queue.dequeue()); // 3
+queue.enqueue(4);
+console.log(queue.peek()); // 4
+queue.enqueue(5);
+queue.enqueue(6);
+queue.enqueue(7);
+queue.enqueue(8);
+queue.enqueue(9); // error
+
